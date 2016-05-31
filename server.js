38f39acd26d445cdffbc961ce9391b65fa3461c5
BlairMcMillan <theblairmcmillan@gmail.com>
2016-05-30 21:01:24 -0500

    moved server to express now working

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
-var path = require('path');
+"use strict";
+
 var express = require('express');
+var path = require('path');
+var cons = require('consolidate');
+
 
 var app = express();
 
-var staticPath = path.resolve(__dirname, '/public');
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+//app.set('views', path.join(__dirname, 'views'));
+//app.engine('.html', require('jade'));
+
+var staticPath = path.join(__dirname, 'public');
 app.use(express.static(staticPath));
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/musician', function(req, res){
+	res.render('musician');
+});
+
+app.get('/dev', function(req, res){
+	res.render('dev');
+});
 
 app.listen(3000, function() {
   console.log('listening');
